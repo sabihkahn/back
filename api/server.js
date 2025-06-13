eab307979c9473d92f75a4cca9ef562ec7ea1f6c
sabihkahn <whyonlyhead@gmail.com>
2025-06-13 13:01:28 +0500

    yes

--- api/server.js
+++ api/server.js
@@ -131,10 +131,6 @@ app.delete("/delete-product/:id", async (req, res) => {
 
 
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
 
 
 export default app;
